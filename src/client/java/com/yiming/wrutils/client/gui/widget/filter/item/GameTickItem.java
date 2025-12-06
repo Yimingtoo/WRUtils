94204d819f86349f3ec54e5f8e9fe269b57a0ae4
Yi Ming <65110940+Yimingtoo@users.noreply.github.com>
2025-12-06 10:23:46 +0800

    filter

---
+++ src/client/java/com/yiming/wrutils/client/gui/widget/filter/item/GameTickItem.java
@@ -0,0 +1,11 @@
+package com.yiming.wrutils.client.gui.widget.filter.item;
+
+public class GameTickItem implements FilterType {
+    String name;
+
+
+    @Override
+    public String getName() {
+        return "";
+    }
+}
---
+++ src/client/java/com/yiming/wrutils/client/gui/widget/filter/item/SequenceItem.java
@@ -0,0 +1,8 @@
+package com.yiming.wrutils.client.gui.widget.filter.item;
+
+public class SequenceItem implements FilterType {
+    @Override
+    public String getName() {
+        return "";
+    }
+}
